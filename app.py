f56c34dac258d9f3ffee2130b6bd5eb66fd0cf77
engumairahmed <eng.umair.a@gmail.com>
2025-08-06 00:28:53 +0500

    run.py changed to app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
 import socket
+from dotenv import load_dotenv
 from app import create_app
+
+load_dotenv()
 
 def find_available_port(start_port=5000):
     port = start_port
@@ -15,4 +19,9 @@ app = create_app()
 
 if __name__ == '__main__':
     port = find_available_port(5000)
-    app.run(debug=True, port=port, use_reloader=False)
+    if __name__ == "__main__":
+        host = os.environ.get('HOST', '0.0.0.0')
+        port = int(os.environ.get('PORT', 5000))
+        
+        app.run(host=host, port=port, debug=True)
+        app.run(debug=True, port=port, use_reloader=False)              
