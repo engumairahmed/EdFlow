194317197e6d36e50ebd07f0084f9b28979d77f4
engumairahmed <eng.umair.a@gmail.com>
2025-08-06 01:28:25 +0500

    Changed app.pu to run.py
    Application is hosted on render using wsgi.py, for local testing run.py is used

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import os
 import socket
 from dotenv import load_dotenv
@@ -19,8 +20,5 @@ app = create_app()
 
 if __name__ == '__main__':
     port = find_available_port(5000)
-    if __name__ == "__main__":
-        host = os.environ.get('HOST', '0.0.0.0')
-        port = int(os.environ.get('PORT', 5000))
-        
-        app.run(host=host, port=port, debug=True, use_reloader=False)
+    host = os.environ.get('HOST', '0.0.0.0')    
+    app.run(host=host, port=port, debug=True, use_reloader=False)
